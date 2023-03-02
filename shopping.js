c4941b4a2ff526388c5db0c6bff21bb35d6190de
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-02 10:27:27 -0600

    Update shopping.js

--- shopping.js
+++ shopping.js
@@ -7,7 +7,29 @@ function handleSubmit(e) {
     e.preventDefault();
     console.log("submitted");
     const name = e.currentTarget.item.value;
-    console.log(name);
+    if (!name) return;
+    const item = {
+        name,
+        id: Date.now(),
+        complete: false,
+    }
+    items.push(item);
+    console.log(`There are now ${items.length} in your state`);
+    e.target.reset();
+    displayItems();
+}
+
+function displayItems() {
+    const html = items.map((item) => 
+    `<li class="shopping-item">
+    <input type="checkbox">
+    <span class="itemName">${item.name}</span>
+    <button aria-label="Remove ${item.name}">&times;</button>
+    </li>
+    `)
+    .join('');
+    list.innerHTML = html;
+    console.log(html);
 }
 
 shoppingForm.addEventListener("submit", handleSubmit);

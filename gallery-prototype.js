a774b85adfd484dc424b07de5cf86b6659225d39
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-05 11:40:50 -0600

    Update gallery-prototype.js

--- gallery-prototype.js
+++ gallery-prototype.js
@@ -1,14 +1,20 @@
 function Gallery(gallery) {
+
+    // If there is no gallery then throw an error
     if (!gallery) {
         throw new Error(`No gallery Found!`);
     }
 
+
+    // Set the variables
     const images = Array.from(gallery.querySelectorAll(`img`));
     const modal = document.querySelector(`.modal`);
     const prevButton = modal.querySelector(`.prev`);
     const nextButton = modal.querySelector(`.next`);
     let currentImage;
 
+
+    // Handle opening the modal
     function openModal() {
         console.info('Opening Modal...');
         // First check if the modal is already open
@@ -19,17 +25,19 @@ function Gallery(gallery) {
         modal.classList.add('open');
     }
 
+    // If someone clicks outside then close the modal
     function handleClickOutside(e) {
         if (e.target === e.currentTarget) {
             closeModal();
         }
     }
-
+    // Close Modal
     function closeModal() {
         console.info(`Closing Modal...`);
         modal.classList.remove(`open`);
     }
 
+    // Update the image in the modal and change the text to match
     function showImage(el) {
         if(!el) {
             console.info(`No image to show`);
@@ -44,9 +52,12 @@ function Gallery(gallery) {
         openModal();
     }
 
+
+    // Event Listeners
     images.forEach(image => image.addEventListener(`click`, (e)=>showImage(e.currentTarget)));
     modal.addEventListener(`click`, handleClickOutside);
 }
 
+// Display gallery to page
 const gallery1 = Gallery(document.querySelector(`.gallery1`));
 const gallery2 = Gallery(document.querySelector(`.gallery2`));

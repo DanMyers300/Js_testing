29650b42313cb2b70f1fc89c9dfca45af3e8f4a2
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-05 11:15:50 -0600

    Update gallery-prototype.js

--- gallery-prototype.js
+++ gallery-prototype.js
@@ -2,6 +2,20 @@ function Gallery(gallery) {
     if (!gallery) {
         throw new Error(`No gallery Found!`);
     }
+
+    const images = Array.from(gallery.querySelectorAll(`img`));
+    const modal = document.querySelector(`.modal`);
+    const prevButton = modal.querySelector(`.prev`);
+    const nextButton = modal.querySelector(`.next`);
+
+    function showImage(el) {
+        if(!el) {
+            console.info(`No image to show`);
+            return;
+        } console.log(el);
+    }
+
+    images.forEach(image => image.addEventListener(`click`, (e)=>showImage(e.currentTarget)));
 }
 
 const gallery1 = Gallery(document.querySelector(`.gallery1`));

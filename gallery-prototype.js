3fedda24513ecf26b69a3e37a86af72e69434604
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-05 11:02:03 -0600

    Update gallery-prototype.js

--- gallery-prototype.js
+++ gallery-prototype.js
@@ -1,1 +1,8 @@
+function Gallery(gallery) {
+    if (!gallery) {
+        throw new Error(`No gallery Found!`);
+    }
+}
 
+const gallery1 = Gallery(document.querySelector(`.gallery1`));
+const gallery2 = Gallery(document.querySelector(`.gallery2`));

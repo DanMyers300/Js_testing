ff6053f45e825977acdc875e64838214e5ce7a82
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-08 19:29:36 -0600

    Update script.js

--- Async_Prompts/script.js
+++ Async_Prompts/script.js
@@ -3,7 +3,15 @@ function ask(options) {
 
         function wait(ms = 0) {
             return new Promise(resolve => setTimeout(resolve, ms));
-          }
+        }
+
+        async function destroyPopup(popup) {
+            let myPopup = popup;
+            popup.classList.remove(`open`);
+            await wait(1000);
+            popup.remove();
+            myPopup = null;
+        }
 
         const popup = document.createElement(`form`);
         popup.classList.add(`popup`);
@@ -21,13 +29,51 @@ function ask(options) {
             const skipButton = document.createElement(`button`);
             skipButton.type = `button`;
             skipButton.textContent = `Cancel`;
+            popup.firstElementChild.appendChild(skipButton);
+            skipButton.addEventListener(
+                'click',
+                function() {
+                  resolve(null);
+                  destroyPopup(popup);
+                },
+                {once:true}
+              );
         }
 
         document.body.appendChild(popup);
         await wait(50);
         popup.classList.add(`open`);
 
+        popup.addEventListener(`submit`, function(e) {
+            e.preventDefault();
+            console.log(e.target.input.value);
+            destroyPopup(popup);
+        }, { once: true });
 });
 }
 
-ask({title: ' does this work'})
+async function askQuestion(e){
+        const button = e.currentTarget;
+    const shouldCancel = 'cancel' in button.dataset;
+    const answer = await ask({
+      title: button.dataset.question,
+      cancel: shouldCancel
+    });
+    questions.forEach(async function() {
+        console.log('Asking a question');
+        console.log(question);
+        const answer = await ask(question);
+        console.log(answer);
+      })
+      async function askMany() {
+        for (const question of questions) {
+          const answer = await ask(question);
+          console.log(answer);
+        }
+      }
+      askMany();
+};
+
+
+const buttons = document.querySelectorAll(`[data-question]`);
+buttons.forEach(button => button.addEventListener(`click`, askQuestion));

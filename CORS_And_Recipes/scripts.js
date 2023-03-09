a57e417d01068c8db2fde90e7671654c4f46bc1d
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-09 17:33:22 -0600

    Update scripts.js

--- CORS_And_Recipes/scripts.js
+++ CORS_And_Recipes/scripts.js
@@ -1,8 +1,25 @@
 const baseEndpoint = "https://recipes.beginnerjavascript.com/api";
+const proxy = "https://cors-anywhere.herokuapp.com/";
+const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query) {
-    const res = await fetch(`https://cors-anywhere.herokuapp.com/${baseEndpoint}?q=${query}`);
+    const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
     const data = await res.json();
+    return data;
 }
 
-fetchRecipes(`pizza`);
+async function handleSubmit(event) {
+    event.preventDefaut();
+    console.log(form.query.value);
+    const el = event.currentTarget;
+    // turn the form off
+    el.submit.disabled = true;
+    // submit the search
+    const recipes = await fetchRecipes(el.query.value);
+    console.log(recipes);
+    el.submit.disabled = false;
+  }
+
+form.addEventListener(`submit`, handleSubmit);
+fetchRecipes("pizza");

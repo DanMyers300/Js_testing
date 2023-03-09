10b439e866e4c21bef551509b75eca1249677d91
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-09 16:17:37 -0600

    Update scripts.js

--- CORS_And_Recipes/scripts.js
+++ CORS_And_Recipes/scripts.js
@@ -1,7 +1,7 @@
 const baseEndpoint = "https://recipes.beginnerjavascript.com/api";
 
 async function fetchRecipes(query) {
-    const res = await fetch(`${baseEndpoint}?q=${query}`);
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${baseEndpoint}?q=${query}`);
     const data = await res.json();
 }
 

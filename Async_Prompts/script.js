1bb62f8b32685e6e41c1da05b89782c4dadfe101
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-08 19:03:24 -0600

    Create script.js

---
+++ Async_Prompts/script.js
@@ -0,0 +1,33 @@
+function ask(options) {
+    return new Promise(async function(resolve) {
+
+        function wait(ms = 0) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+          }
+
+        const popup = document.createElement(`form`);
+        popup.classList.add(`popup`);
+        popup.insertAdjacentHTML(
+            "afterbegin", `
+              <fieldset>
+                <label>${options.title}</label>
+                <input type="text" name="input" />
+                <button type="submit">Submit</button>
+              </fieldset>
+            `
+          );
+
+        if (options.cancel) {
+            const skipButton = document.createElement(`button`);
+            skipButton.type = `button`;
+            skipButton.textContent = `Cancel`;
+        }
+
+        document.body.appendChild(popup);
+        await wait(50);
+        popup.classList.add(`open`);
+
+});
+}
+
+ask({title: ' does this work'})

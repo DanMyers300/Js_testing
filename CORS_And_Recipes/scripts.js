1e9d037c2919bac55b3a44a1436f584f2accee88
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-09 16:07:39 -0600

    Update scripts.js

--- CORS_And_Recipes/scripts.js
+++ CORS_And_Recipes/scripts.js
@@ -1,1 +1,8 @@
-const recipePuppy = "https://recipes.beginnerjavascript.com/api"
+const baseEndpoint = "https://recipes.beginnerjavascript.com/api";
+
+async function fetchRecipes(query) {
+    const res = await fetch(`${baseEndpoint}?q=${query}`);
+    const data = await res.json();
+}
+
+fetchRecipes(`pizza`);
